14eed415ae8a4e88a17cb44249c41334013dc36e
Artur Galstyan <galstyan.artu@gmail.com>
2022-07-23 11:12:13 +0200

    finished blog post

--- demos/demos.py
+++ demos/demos.py
@@ -7,3 +7,8 @@ demos_bp = Blueprint("demos_bp", __name__, template_folder="templates")
 @demos_bp.route("/")
 def home():
     return render_template("demos/index.html")
+
+
+@demos_bp.route("/neural_network_mnist")
+def neural_network_mnist():
+    return render_template("demos/neural_network_mnist.html")
